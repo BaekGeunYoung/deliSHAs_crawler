289cd5087a4595d0da66d53b9054c0e2a2e9b15f
BaekGeunYoung <dvmflstm@naver.com>
2020-03-16 00:31:43 +0900

    환경에 따라 바뀔 수 있는 값들을 환경변수화함.

--- src/main/kotlin/Constants.kt
+++ src/main/kotlin/Constants.kt
@@ -1,9 +1,9 @@
 class Constants {
     companion object {
-        const val BASE_CRAWL_URL = "http://snuco.snu.ac.kr/ko/foodmenu"
-        const val JDBC_URL = "jdbc:mysql://localhost:3306/test_db?serverTimezone=UTC&characterEncoding=UTF-8"
-        const val JDBC_DRIVER = "com.mysql.cj.jdbc.Driver"
-        const val JDBC_USERNAME = "root"
-        const val JDBC_PASSWORD = "dkdltm123"
+        val BASE_CRAWL_URL = System.getenv("BASE_CRAWL_URL") ?: "http://snuco.snu.ac.kr/ko/foodmenu"
+        val JDBC_URL = System.getenv("JDBC_URL") ?: "jdbc:mysql://localhost:3306/test_db?serverTimezone=UTC&characterEncoding=UTF-8"
+        val JDBC_DRIVER = System.getenv("JDBC_DRIVER") ?: "com.mysql.cj.jdbc.Driver"
+        val JDBC_USERNAME = System.getenv("JDBC_USERNAME") ?: "root"
+        val JDBC_PASSWORD = System.getenv("JDBC_PASSWORD") ?: "dkdltm123"
     }
 }
