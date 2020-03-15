efdccf9dd2fd37efa5fd6ce0f676e388d813b4e8
BaekGeunYoung <dvmflstm@naver.com>
2020-03-15 20:57:27 +0900

    mysql 사용을 위해 hikariConfig에 driverClassName 추가

--- src/main/kotlin/DataSource.kt
+++ src/main/kotlin/DataSource.kt
@@ -2,14 +2,17 @@ import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 
 class DataSource {
-    private val config = HikariConfig()
-    private val dataSource = HikariDataSource(config)
+    private var config : HikariConfig = HikariConfig()
+    private var dataSource : HikariDataSource
 
     fun getConnection() = dataSource.connection
 
     init {
         config.jdbcUrl = Constants.JDBC_URL
+        config.driverClassName = Constants.JDBC_DRIVER
         config.username = Constants.JDBC_USERNAME
         config.password = Constants.JDBC_PASSWORD
+
+        dataSource = HikariDataSource(config)
     }
 }
