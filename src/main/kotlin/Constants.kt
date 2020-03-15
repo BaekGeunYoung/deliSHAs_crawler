cf79dd024fc8a3892c6b71818d24bd05f87c7731
BaekGeunYoung <dvmflstm@naver.com>
2020-03-15 20:56:46 +0900

    mysql connection에 필요한 상수들 정의

--- src/main/kotlin/Constants.kt
+++ src/main/kotlin/Constants.kt
@@ -1,8 +1,9 @@
 class Constants {
     companion object {
         const val BASE_CRAWL_URL = "http://snuco.snu.ac.kr/ko/foodmenu"
-        const val JDBC_URL = ""
+        const val JDBC_URL = "jdbc:mysql://localhost:3306/test_db?serverTimezone=UTC&characterEncoding=UTF-8"
+        const val JDBC_DRIVER = "com.mysql.cj.jdbc.Driver"
         const val JDBC_USERNAME = "root"
-        const val JDBC_PASSWORD = "password"
+        const val JDBC_PASSWORD = "dkdltm123"
     }
 }
