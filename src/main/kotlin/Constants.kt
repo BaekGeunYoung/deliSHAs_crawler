8d659ac2c061f237ab97fc4379a777514efdd1d2
BaekGeunYoung <dvmflstm@naver.com>
2020-04-11 18:50:13 +0900

    connection url의 serverTimeZone을 asia/seoul로 변경

--- src/main/kotlin/Constants.kt
+++ src/main/kotlin/Constants.kt
@@ -2,7 +2,7 @@ class Constants {
     companion object {
         val BASE_CRAWL_URL: String = System.getenv("BASE_CRAWL_URL") ?: "http://snuco.snu.ac.kr/ko/foodmenu"
         private val JDBC_HOST: String = System.getenv("JDBC_URL") ?: "localhost"
-        val JDBC_URL: String = "jdbc:mysql://${this.JDBC_HOST}:3306/BapMukSha?serverTimezone=UTC&characterEncoding=UTF-8"
+        val JDBC_URL: String = "jdbc:mysql://${this.JDBC_HOST}:3306/BapMukSha?serverTimezone=Asia/Seoul&characterEncoding=UTF-8"
         val JDBC_DRIVER: String = System.getenv("JDBC_DRIVER") ?: "com.mysql.cj.jdbc.Driver"
         val JDBC_USERNAME: String = System.getenv("JDBC_USERNAME") ?: "root"
         val JDBC_PASSWORD: String = System.getenv("JDBC_PASSWORD") ?: "dkdltm123"
